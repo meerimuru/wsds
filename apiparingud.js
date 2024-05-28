a7cfff179c5772d607aca082edab73d4d4aaab2a
meeri <meerimuru@gmail.com>
2024-05-29 00:15:43 +0300

    uued lingid

--- apiparingud.js
+++ apiparingud.js
@@ -122,15 +122,15 @@ async function handleResponse(form, responseData) {
 
 async function listiraamatud() {
     try {
-        const responseData = await getDataAsJson("http://localhost:5000/raamatud/");
+        const responseData = await getDataAsJson("https://wsdswebapp.azurewebsites.net/raamatud/");
 
         const resultElement = document.getElementById("raamatud_result");
         resultElement.innerHTML = "";
 
         for (var raamat of responseData.raamatud) {
             raamat = raamat.split('.')[0]; // Corrected variable name
-            resultElement.innerHTML += `<a href="http://localhost:5000/raamatud/${raamat}" download="${raamat}.txt">${raamat}.txt</a> ` +
-                `<a href="#" onclick="deleteObject('http://localhost:5000/raamatud/${raamat}')">[kustuta]</a>` +
+            resultElement.innerHTML += `<a href="https://wsdswebapp.azurewebsites.net/raamatud/${raamat}" download="${raamat}.txt">${raamat}.txt</a> ` +
+                `<a href="#" onclick="deleteObject('https://wsdswebapp.azurewebsites.net/raamatud/${raamat}')">[kustuta]</a>` +
                 "<br />";
         }
     } catch (error) {
